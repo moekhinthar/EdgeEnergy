11ef8821fc213dc31b300e740ee16ee7d69dc46a
moekhinthar <moekhinthar@users.noreply.github.com>
2019-06-05 20:42:56 +1000

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EdgeEnergy.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:ListBox ID="ListBox1" runat="server" Height="197px" Width="146px"></asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnLeft" runat="server" OnClick="btnLeft_Click" Text="&lt;&lt;" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnRight" runat="server" OnClick="btnRight_Click" Text="&gt;&gt;" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox2" runat="server" Height="194px" Width="158px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+        <br />
+        <br />
+        <asp:TextBox ID="txtSubmit" runat="server"></asp:TextBox>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace EdgeEnergy
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+                doc.Load(@"C:\Users\panmo\source\repos\EdgeEnergy\App_Data\XMLFile1.xml");
+                System.Xml.XmlElement root = doc.DocumentElement;
+                System.Xml.XmlNodeList lst = root.GetElementsByTagName("list");
+
+                foreach (System.Xml.XmlNode n in lst)
+                {
+                    ListBox1.Items.Add(n.InnerText);
+                }
+            }
+        }
+
+        protected void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (ListBox2.SelectedIndex != -1)
+            {
+                ListBox1.Items.Add(ListBox2.SelectedValue);
+                ListBox2.Items.Remove(ListBox2.SelectedValue);
+            }
+        }
+
+        protected void btnRight_Click(object sender, EventArgs e)
+        {
+            if (ListBox1.SelectedIndex != -1)
+            {
+                ListBox2.Items.Add(ListBox1.SelectedValue);
+                ListBox1.Items.Remove(ListBox1.SelectedValue);
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ListBox2.Items.Count; i++)
+            {
+                if ((i + 1) < ListBox2.Items.Count)
+                    txtSubmit.Text += ListBox2.Items[i] + ",";
+                else
+                    txtSubmit.Text += ListBox2.Items[i];
+            }
+        }
+    }
+
+
+
+}
