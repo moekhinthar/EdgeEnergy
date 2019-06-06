4e80e335ebc522b7b8f374ce3e92b0b385ea5094
moekhinthar <moekhinthar@users.noreply.github.com>
2019-06-06 16:45:37 +1000

    Add files via upload
    Category.cs file under Models folder
    HomeController.cs under Controllers folder
    INdex.cshtml under Views >> Home directory

---
+++ Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EdgeEnergy_MVC.Models
+{
+    public class Category
+    {
+        public string CatId { get; set; }
+        public string CatName { get; set; }
+    }
+    public class CategoryList
+    {
+        public List<Category> ListBox1 { get; set; }
+        public List<Category> ListBox2 { get; set; }
+        public string[] SelectedCategoryName { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EdgeEnergy_MVC.Models;
+
+namespace EdgeEnergy_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CategoryList categoryList = new CategoryList();
+            categoryList.ListBox1 = new List<Category>();
+            categoryList.ListBox2 = new List<Category>();
+            categoryList.ListBox1 = GetCatData();
+
+            return View(categoryList);
+        }
+
+        public List<Category> GetCatData()
+        {
+            List<Category> categorylist = new List<Category>();
+
+            var consumed = from nodes in System.Xml.Linq.XElement.Load(Server.MapPath(@"/Categories.xml")).Elements("List") select nodes;
+
+            foreach (var b in consumed)
+            {
+                string s = b.Element("ID").Value.Trim();
+                string t = b.Element("Name").Value.Trim();
+                categorylist.Add(new Category { CatId = s, CatName = t });
+            }
+
+            return categorylist;
+        }
+
+        [HttpPost]
+        public ActionResult Index(CategoryList categorylist)
+        {
+            string SelectedCategoryName = "";
+            
+            categorylist.ListBox1 = GetCatData().Where(m => !categorylist.SelectedCategoryName.Contains(m.CatName)).ToList();
+            categorylist.ListBox2 = GetCatData().Where(m => categorylist.SelectedCategoryName.Contains(m.CatName)).ToList();
+            
+            foreach (string item in categorylist.SelectedCategoryName)
+            {
+                SelectedCategoryName = SelectedCategoryName + item + ",";
+            }
+
+            ViewBag.SelectedCategoryName = SelectedCategoryName;
+
+            return View(categorylist);
+        }
+    }
+}
